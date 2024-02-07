6596b5e1dd7c02e6fef869c26f342420d1560cad
Maria <zeldacamila0913@gmail.com>
2024-02-07 17:14:51 -0500

    modified:   index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const bodyParser = require('body-parser');
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
